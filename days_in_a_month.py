2c1d143ff666f41d61f548535bdc33f2292be82c
Ubuntu <ubuntu@ip-172-31-30-141.ec2.internal>
2019-10-29 21:49:37 +0000

    days in a month

---
+++ days_in_a_month.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+# Created by:Euel Yirga
+# Created on:October 2019
+# This program is a while loop
+
+def main():
+    # input
+    month = input("Please enter the name of a month: ")
+
+    # process
+    if month== "January":
+        # output
+        print()
+        print("31")
+
+    # process
+    elif month == "February":
+        # output
+        print()
+        print("28")
+
+    # process
+    elif month == "March": 
+        # output
+        print()
+        print("31")
+
+    #process
+    elif month == "April":
+        #output
+        print()
+        print("30")
+        
+    #process
+    elif month == "May":
+        #output
+        print()
+        print("31")
+        
+    #process
+    elif month == "June":
+        #output
+        print()
+        print("30")
+        
+    #process
+    elif month == "July":
+        #output
+        print()
+        print("31")
+        
+    #process
+    elif month == "August":
+        #output
+        print()
+        print("31")
+        
+    #process
+    elif month == "September":
+        #output
+        print()
+        print("30")
+    
+    #process    
+    elif month == "October":
+        #output
+        print()
+        print("31")
+        
+    #process
+    elif month == "November":
+        #output
+        print()
+        print("30")
+        
+    #process
+    elif month == "December":
+        #output
+        print()
+        print("31")
+
+if __name__ == "__main__":
+    main()
